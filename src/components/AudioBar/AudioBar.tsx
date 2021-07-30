d6af4f89973518c06cfbdd661b78f8ccbfbb4224
Oscar Zealley <ozealley@twilio.com>
2021-07-30 13:03:19 +0100

    initial commit

---
+++ src/components/AudioBar/AudioBar.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import { AudioBarStyles } from "./AudioBar.Styles";
+import { AudioPlayerError } from "@twilio/flex-ui";
+import { AudioPlayerManager } from "@twilio/flex-ui";
+import { Howl } from "howler";
+
+const CustomTaskList = () => {
+  const playAudioManagerSound = () => {
+    AudioPlayerManager.play(
+      {
+        url: "https://cicct-flex-plugin-core-9515-production.twil.io/auto-answer-chime.mp3",
+        repeatable: false,
+      },
+      (error: AudioPlayerError) => {
+        console.log(error);
+      }
+    );
+  };
+
+  const playHowlerSound = () => {
+    var sound = new Howl({
+      src: "https://cicct-flex-plugin-core-9515-production.twil.io/auto-answer-chime.mp3",
+      sprite: {
+        intro: [0, 20000],
+      },
+      autoplay: false,
+      loop: false,
+      html5: true,
+    });
+    sound.play("intro");
+  };
+
+  return (
+    <AudioBarStyles>
+      <button onClick={playAudioManagerSound}>Play AudioManager Sound</button>
+      <button onClick={playHowlerSound}>Play Howler Sound</button>
+    </AudioBarStyles>
+  );
+};
+
+export default CustomTaskList;
